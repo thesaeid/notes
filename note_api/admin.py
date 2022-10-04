1c10d0363a8a2d08a8d2317f7430f2694bfa8e30
Saeid Heidari <saeiiid.heidari@gmail.com>
2022-10-04 14:52:13 +0330

    add admin for Note and Profile models.

--- note_api/admin.py
+++ note_api/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Note, Profile
 
-# Register your models here.
+
+class NoteInline(admin.StackedInline):
+    model = Note
+    extra = 1
+
+
+@admin.register(Note)
+class NoteAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "short_content", "creation_date", "update_date")
+    readonly_fields = ("creation_date", "update_date")
+    list_display_links = ("id", "title")
+
+    search_fields = ("title", "content")
+    list_filter = ("creation_date", "update_date")
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ("user", "bio", "birth_date")
+    list_filter = ("user", "bio", "birth_date")
+    list_select_related = ["user"]
+    inlines = [NoteInline]
