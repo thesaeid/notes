06bec42e15ca6daec4d965988aafd8b7c2c3e239
Saeid Heidari <saeiiid.heidari@gmail.com>
2022-10-04 14:51:33 +0330

    configure models

--- note_api/models.py
+++ note_api/models.py
@@ -1,25 +1,30 @@
+from uuid import uuid4
 from django.db import models
 from django.conf import settings
+from django.template.defaultfilters import truncatechars
+
+
+class Profile(models.Model):
+    id = models.UUIDField(default=uuid4, primary_key=True)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True)
+    birth_date = models.DateField(null=True, blank=True)
+    # notes = models.ForeignKey(Note, on_delete=models.CASCADE, related_name="profile")
+
+    def __str__(self):
+        return self.user.username
 
 
 class Note(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField()
-    owner = models.ForeignKey(
-        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="notes"
-    )
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name="notes")
 
     creation_date = models.DateTimeField(auto_now_add=True)
     update_date = models.DateTimeField(auto_now=True)
 
+    def short_content(self):
+        return truncatechars(self.content, 20)
+
     def __str__(self):
         return self.title
-
-
-class Profile(models.Model):
-    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    bio = models.TextField(blank=True)
-    birth_date = models.DateField(null=True, blank=True)
-
-    def __str__(self):
-        return self.user.username
